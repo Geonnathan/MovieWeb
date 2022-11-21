28e31e2b36e86ccb8e2a5a53bb15cee93994b96e
Geonnathan <geonnathan1494ge@hotmail.com>
2022-11-21 13:15:02 -0600

    second

--- script.js
+++ script.js
@@ -1,1 +1,148 @@
 //TMDB
+
+const API_KEY = 'api_key=9a62bb9d4070044c546245e9306ccdd4';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL+ '/search/movie?'+API_KEY;
+
+const genres = [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+getMovies(API_URL);
+
+function getMovies(url){
+    fetch(url).then(res=> res.json()).then(data=> {
+        showMovies(data.results);
+    })
+
+}
+
+function showMovies(data){
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview}= movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <div class="movie">
+                <img src="${IMG_URL+poster_path}" alt="${title}">
+                <div class="movie-info">
+                    <h3>${title}</h3>
+                    <span class="${getColor(vote_average)}">${vote_average}</span>
+                </div>
+                <div class="overview">
+                    <h3>overview</h3>
+                    ${overview}
+                </div>
+            </div>
+        `
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getColor(vote){
+    if(vote>=8){
+        return'green';
+    }else if (vote >= 5){
+        return 'orange'
+    }else{
+        return 'red'
+    }
+
+}
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    if(searchTerm){
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+
+})
